7f6a4af80d11cd48cbdec735b26e8261e063d15d
Brett Cunningham <bwcunninghamii@gmail.com>
2019-05-07 19:08:18 -0400

    able to make post request

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const path = require('path')
 const cors = require('cors');
+var bodyParser = require('body-parser');
 let { morningTexts } = require('../model/messagesArchive.js');
 let { morningConvoStarters } = require('../model/messagesArchive.js');
 
@@ -8,11 +10,19 @@ const app = express()
 const port = 3000
 
 app.use(cors())
+app.use(bodyParser.json())
+
+app.use(express.static(path.join(__dirname, '../dist')))
+
 
 app.get('/message', (req, res) => {
   // res.send('Hello World!')
   res.send(morningTexts[getRandomInt(10)])
 })
+
+app.post('/message', (req, res) => {
+  console.log('req.body: ', req.body);
+});
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
